d943158abb693a233598d90797726de070b98dc6
bear30921 <bear30921@gmail.com>
2017-10-11 13:30:38 +0800

    add validation to animal information

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,17 +1,20 @@
 class Cat < ApplicationRecord
   belongs_to :user
-
   has_many :favorites, dependent: :destroy
   has_many :favorited_by, through: :favorites, source: :user 
 
-
-
   has_attached_file :avatar, styles: { medium: "400x400>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-
-
   validates :avatar, attachment_presence: true
   
+
+  def gender(gender)
+    if gender == 1
+      "男"
+    else
+      "女"
+    end
+  end
 
 
 end
