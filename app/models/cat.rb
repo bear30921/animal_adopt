d236f76b27cdbd78b860ec64e5c4ba230881e908
bear30921 <bear30921@gmail.com>
2017-10-14 20:17:50 +0800

    using PostgreSQL in environment、fix bug in search

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -5,7 +5,7 @@ class Cat < ApplicationRecord
 
   has_attached_file :avatar, styles: { medium: "400x400>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-  validates :avatar, attachment_presence: true
+  validates_presence_of :avatar, :name, :age, :city, :description
   
 
   def gender(gender)
